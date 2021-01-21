083ef7c1238367e9d5b93a8d7558b529ed6cc617
Emmanuel Nwamah <emmanuel.nwamah@kmeleon.tech>
2021-01-21 17:07:51 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+export interface PeriodicElement {
+  FirstName: string;
+  LastName: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {FirstName: 'Emmanuel', LastName: 'Nwamah'},
+  {FirstName: '1', LastName: '2'}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'form-app';
+  displayedColumns: string[] = ['FirstName', 'LastName'];
+  dataSource = ELEMENT_DATA;
+
+
 }
